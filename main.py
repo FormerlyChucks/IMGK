483aa62884e16031c21247d5147200cf5f273152
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-06-21 21:23:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,8 @@ API_TOKEN = 'your api token'
 url = 'https://api.imgur.com/3/account/{}/settings'.format(USERNAME)
 headers = {'Authorization': 'Bearer {}'.format(API_TOKEN)}
 
-while True:
-    try:
-        with open("lines.txt") as f:
-            lines = [l.rstrip() for l in f]
-            parameters = {'bio': random.choice(lines)}
-            response = requests.put(url, headers=headers, params=parameters)
-        print('Updated Imgur Bio!')
-        time.sleep(300)
-    except Exception:
-        print(traceback.format_exc())
-        time.sleep(60)
+with open("lines.txt") as f:
+    lines = [l.rstrip() for l in f]
+
+parameters = {'bio': random.choice(lines)}
+response = requests.put(url, headers=headers, params=parameters)
